47a212099ce61a61b8356cb5ddc5bb96864aca8f
William Sanders <253146473+wsandersvc@users.noreply.github.com>
2026-01-28 13:04:02 -0600

    sec: remove vulnerable code

--- badguys/vulnerable/views.py
+++ badguys/vulnerable/views.py
@@ -67,17 +67,17 @@ def code_execution(request):
 
         first_name = request.POST.get('first_name', '')
 
-        # try:
-        #     # Try it the Python 3 way...
-        #     exec(base64.decodestring(bytes(first_name, 'ascii')))
-        # except TypeError:
-        #     # Try it the Python 2 way...
-        #     try:
-        #         exec(base64.decodestring(first_name))
-        #     except:
-        #         pass
-        # except:
-        #     pass
+        try:
+            # Try it the Python 3 way...
+            base64.decodestring(bytes(first_name, 'ascii'))
+        except TypeError:
+            # Try it the Python 2 way...
+            try:
+                base64.decodestring(first_name)
+            except:
+                pass
+        except:
+            pass
 
         # Check to see if the attack was successful
         try:
